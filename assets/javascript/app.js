3c215f2dbba2d64142272f596c7e3b96b73006b8
vsaraogi <mailvsaraogi@gmail.com>
2018-06-21 17:23:03 -0400

    Now showing questions randomly. Also made code more modular

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,9 +4,10 @@
 "use strict";
 
 // Max time in seconds
-const MAX_TIME = 5;
+const QUESTION_NUM  = 6;
+const MAX_TIME = 5 * QUESTION_NUM;
 
-const triviaQuestions =
+const QUESTIONS =
 {
     "nj-capital": {
         question: "Capital of New Jersey",
@@ -28,13 +29,293 @@ const triviaQuestions =
         ],
         correctChoice: "Albany"
     },
-}
 
-// VIK_QUESTION: How to assert that keys are unique
-// VIK_TODO: Wrap it somewhere 
-var questionKeys = []; 
-var buildKeyArray = () => { for (let i in triviaQuestions) questionKeys.push(i); }
-buildKeyArray();
+    "al-capital": {
+        question: "Capital of Alabama",
+        choices: [
+            "Mobile",
+            "Birigham",
+            "Little Rock",
+            "Springfield",
+        ],
+        correctChoice: "Birigham"
+    },
+    "ak-capital": {
+        question: "Captal of Alaska",
+        choices: [
+            "Denali",
+            "Juneau",
+            "Anchorage",
+            "Ketchikan",
+            
+        ],
+        correctChoice: "Juneau"
+    },
+
+        "az-capital": {
+        question: "Capital of Arizona",
+        choices: [
+            "Pheonix",
+            "Sedona" ,
+            "Tucson",
+            "Scottsdale",
+
+        ],
+        correctChoice: "Pheonix"
+    },
+    "ca-capital": {
+        question: "Capital of California",
+        choices: [
+            "Berkeley",
+            "Sacramento",
+            "Palo Alto",
+            "Los Angeles",
+        ],
+        correctChoice: "Sacramento"
+    },
+    "fl-capital": {
+        question: "Capital of Flordia",
+        choices: [
+            "Tallahassee",
+            "Tampa",
+            "Destin",
+            "Jacsonville",
+        ],
+        correctChoice: "Tallahassee"
+    },
+    "tx-capital": {
+        question: "Capital of Texas",
+        choices: [
+            "Houston",
+            "Dallas",
+            "Austin",
+            "San Antonio",
+        ],
+        correctChoice: "Austin"
+    },
+
+    "sc-capital": {
+        question: "Capital of South Carolina",
+        choices: [
+            "Columbia",
+            "Greenville",
+            "Chareston",
+            "Lexington",
+        ],
+        correctChoice: "Columbia"
+    },
+
+    "nc-capital": {
+        question: "Capital of North Carolina",
+        choices: [
+            "Raleigh",
+            "Charolette",
+            "Greensboro",
+            "Wilmington",
+        ],
+        correctChoice: "Raleigh"
+    },
+ 
+    "ks-capital": {
+        question: "Capital of Kansas",
+        choices: [
+            "Kansas City",
+            "Wichita",
+            "Lawrence",
+            "Topeka",
+        ],
+        correctChoice: "Topeka"
+    },
+
+    "nb-capital": {
+        question: "Capital of Nebraska",
+        choices: [
+            "Lincoln",
+            "Kearney",
+            "Scottsbluff",
+            "Omaha",
+        ],
+        correctChoice: "Lincoln"
+    },
+
+    "nv-capital": {
+        question: "Capital of Nevada",
+        choices: [
+            "Las Vegas",
+            "Henderson",
+            "Reno",
+            "Carson City",
+        ],
+        correctChoice: "Carson City"
+    },
+
+    "vg-capital": {
+        question: "Capital of Virginia",
+        choices: [
+            "Norfolk",
+            "Williamsburg",
+            "Richmond",
+            "Alexandria",
+        ],
+        correctChoice: "Richmond"
+    },
+
+    "ws-capital": {
+        question: "Capital of Washington",
+        choices: [
+            "Tacoma",
+            "Seattle",
+            "Olympia",
+            "Bellingham",
+        ],
+        correctChoice: "Olympia"
+    },
+
+    "my-capital": {
+        question: "Capital of Maryland",
+        choices: [
+            "Annapolis",
+            "Fredrick",
+            "Baltimore",
+            "Hagerstown",
+        ],
+        correctChoice: "Annapolis"
+    },
+
+    "il-capital": {
+        question: "Capital of Illinois",
+        choices: [
+            "Springfield",
+            "Chicago",
+            "Peoria",
+            "Bloomington",
+        ],
+        correctChoice: "Springfield"
+    },
+
+    "hw-capital": {
+        question: "Capital of Hawaii",
+        choices: [
+            "Honolulu",
+            "Hilo",
+            "Pahoa",
+            "Kailua",
+        ],
+        correctChoice: "Honolulu"
+    },
+
+    "ri-capital": {
+        question: "Capital of Rhode Island",
+        choices: [
+            "Newport",
+            "Westerly",
+            "Providence",
+            "Warwick",
+        ],
+        correctChoice: "Providence"
+    },
+
+    "ts-capital": {
+        question: "Capital of Tennessee",
+        choices: [
+            "Gatlinburg",
+            "Knoxville",
+            "Memphis",
+            "Nashville",
+        ],
+        correctChoice: "Nashville"
+    },
+
+    "og-capital": {
+        question: "Capital of Oregon",
+        choices: [
+            "Salem",
+            "Medford",
+            "Portland",
+            "Bend",
+        ],
+        correctChoice: "Salem"
+    },
+
+    "go-capital": {
+        question: "Capital of Georgia",
+        choices: [
+            "Macon",
+            "Augusta",
+            "Atlanta",
+            "Savannah",
+        ],
+        correctChoice: "Atlanta"
+    },
+}
+ 
+// VIK_TODO: assert that keys are unique
+var TriviaQuestions = new function() {
+    let _quizQuestions = QUESTIONS;
+    let _sessionKeys = [];
+
+    this.questionKeys = function() {
+        // keys of the questions from question object
+        let keys = [];
+        for (let i in _quizQuestions) {
+            keys.push(i);
+        }
+        
+        return keys;
+    }
+
+    // Keys of questions asked during a session
+     this.sessionQuestionKeys = function() {
+         // Already populated
+         if (_sessionKeys.length > 0)
+            return _sessionKeys;
+
+         let qKeys = this.questionKeys();
+        let randomIndices = getUniqueRandomNumbers( {min:0, max: qKeys.length}, 
+            QUESTION_NUM);
+        console.assert(qKeys.length > 0, "question keys is not populated");
+        // Get random question keys using the random array
+        for (let i=0; i < randomIndices.length; ++i) {
+            _sessionKeys.push( qKeys[randomIndices[i]] );
+        }
+        return _sessionKeys;
+    }
+
+    this.getQuestionObj = function(key) {
+        console.assert(this.sessionQuestionKeys().includes(key), key + " is not in questions");
+        if ( _quizQuestions[key] )
+            return _quizQuestions[key];
+    }
+}
+ 
+ 
+function randomGenerator(min, max, roundToNearest)
+{
+    if (roundToNearest === undefined) {
+        roundToNearest = 1;
+    }
+
+    let num = ( min + Math.round(Math.random() * (max-min) ) );
+    return ( Math.round(num/roundToNearest) * roundToNearest );
+}   
+
+// Returns an array containing unique number between a range,
+// including the range min and max. Note min and max can be same
+function getUniqueRandomNumbers(range, size) {
+    let arr = [];
+    size = parseInt(size);
+    console.assert(range.max >= range.min);
+    console.assert(size <= range.max - range.min, "range is less than the required length of array");
+    console.assert(!!size, "passed invalid length argument")
+    while (!!size && (size <= range.max - range.min) && arr.length != size) {
+        let rand = randomGenerator(range.min, range.max);
+        if (!arr.includes(rand)) {
+            arr.push(rand);
+        }
+    }
+    return arr;
+}        
+
 
 $(document).ready(function() {
     initContainer();
@@ -123,19 +404,19 @@ var result = {
 }
 
 function getResult() {
-    for (let i = 0; i < questionKeys.length; ++i) {
-        const qKey = questionKeys[i];
+    let sesQKeys = TriviaQuestions.sessionQuestionKeys();
+    for (let i = 0; i < sesQKeys.length; ++i) {
+        const qKey = sesQKeys[i];
         const qVal = $('input[name=' + qKey + ']:checked').val();
-        let qObj = triviaQuestions[qKey];
-        console.assert(qObj !== undefined, "question object not found for key " + qKey);
+        
+        const qObj = TriviaQuestions.getQuestionObj(qKey);
+        const correctAns = qObj && qObj.correctChoice;
         if (qVal === undefined)
             result.unanswered++;
-        else if (qVal === qObj.correctChoice)
+        else if (qVal === correctAns)
             result.correct++;
         else
             result.incorrect++;
-
-        // console.log(qVal);
     }
 }
 
@@ -147,9 +428,15 @@ function createQuestionsView() {
     let formGroup = $("<div>").attr("class", "form-group");
     formGroup.appendTo(questionListNode);
 
-    for (let i = 0; i < questionKeys.length; ++i) {
-        let qKey = questionKeys[i];
-        let qObj = triviaQuestions[qKey];
+    // Random unique indicies of the question array
+    // let questionKeyIndicies = getUniqueRandomNumbers( {min:0, max:triviaQuestions.questionKeys.length}, QUESTION_NUM);
+    const sesQKeys = TriviaQuestions.sessionQuestionKeys();
+    for (let i = 0; i < sesQKeys.length; ++i) {
+        // Get the index, for which we will get the key and find the 
+        // question object using that key
+        // const index = questionKeyIndicies[i];
+        const qKey = sesQKeys[i];
+        const qObj = TriviaQuestions.getQuestionObj(qKey);
 
         // Build the question element
         let questionNode = $("<div>").
